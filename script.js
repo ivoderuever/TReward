7681c0aff3943199d10e02682b4ff88846e937e8
ivoderuever <ivoderuever@gmail.com>
2021-11-13 16:42:55 +0100

    Add the function to automaticly press it when button apears

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-setInterval(() => {
-    //Reward button element
-    let rewardBtn = document.querySelector(".claimable-bonus__icon");
+// Old sjity code
+// setInterval(() => {
+//     //Reward button element
 
-    if(rewardBtn != null) {
-        rewardBtn.click();
+//     if (rewardBtn != null) {
+//         rewardBtn.click();
+//     }
+// }, 30 * 1000)
+
+let rewardBtn;
+let rewardBar = document.querySelector(".chat-input__buttons-container");
+
+const config = { attributes: true, childList: true, subtree: true };
+
+const callback = function (mutationsList, observer) {
+    for (const mutation of mutationsList) {
+        if (mutation.target.querySelector('.claimable-bonus__icon')) {
+            rewardBtn = document.querySelector(".claimable-bonus__icon");
+            if (rewardBtn != null) {
+                rewardBtn.click();
+            }
+        }
     }
-}, 30 * 1000)
+};
+
+const observer = new MutationObserver(callback);
+
+
+observer.observe(rewardBar, config);

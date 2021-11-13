266a4b3d8a02dc8aced411ca3b060f66b16aae1a
ivoderuever <ivoderuever@gmail.com>
2021-11-13 17:29:59 +0100

    Fixed if connection to element fails it tries again and add a reconnect every minute to check if  connection is stable

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-// Old sjity code
-// setInterval(() => {
-//     //Reward button element
-
-//     if (rewardBtn != null) {
-//         rewardBtn.click();
-//     }
-// }, 30 * 1000)
-
-let rewardBtn;
-let rewardBar = document.querySelector(".chat-input__buttons-container");
+let rewardBar;
+function observe() {
+    setTimeout(() => {
+        try {
+            rewardBar = document.querySelector(".chat-input__buttons-container");
+            observer.observe(rewardBar, config);
+            console.log("Connected to points")
+        } catch (error) {
+            observe();
+        }
+    }, 5000);
+}
 
 const config = { attributes: true, childList: true, subtree: true };
 
 const callback = function (mutationsList, observer) {
     for (const mutation of mutationsList) {
         if (mutation.target.querySelector('.claimable-bonus__icon')) {
-            rewardBtn = document.querySelector(".claimable-bonus__icon");
+            let rewardBtn = document.querySelector(".claimable-bonus__icon");
             if (rewardBtn != null) {
                 rewardBtn.click();
             }
@@ -25,5 +26,10 @@ const callback = function (mutationsList, observer) {
 
 const observer = new MutationObserver(callback);
 
+observe();
 
-observer.observe(rewardBar, config);
+setInterval(() => {
+    console.log("Reconnecting auto claimer");
+    observer.disconnect();
+    observe();
+}, 60 * 1000)
